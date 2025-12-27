5295ffe5788ad5fe60ca54c7507be06a308dcdc9
Noah Moorby <n.moorby@hotmail.com>
2025-12-27 21:43:45 +0000

    Add files via upload

---
+++ Simple number pattern generator.py
@@ -0,0 +1,10 @@
+def number_pattern(n):
+    if not isinstance(n,int):
+        return 'Argument must be an integer value.'
+    elif n < 1:
+        return 'Argument must be an integer greater than 0.'
+    else:
+        return ' '.join([str(i) for i in range(1,n+1)])
+
+
+
