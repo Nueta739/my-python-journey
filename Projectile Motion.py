1d236b7ab0ba9740919d39c434cfd5130e1a1549
Noah Moorby <n.moorby@hotmail.com>
2026-01-02 23:10:32 +0000

    Update Projectile Motion.py

--- Projectile Motion.py
+++ Projectile Motion.py
@@ -2,30 +2,32 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-def projectile_motion(velocity,angle,height = 0, g = 9.81 ):
-    angle = math.radians(angle)
-    flight_time = round((2 * velocity * math.sin(angle))/g,2)
-    range_of_flight = round(velocity * math.cos(angle) * flight_time,2)
-    maximum_height = round(((velocity*math.sin(angle))**2)/(2*g),2)
+v = float(input('Enter the projectiles initial speed: '))
+while v <= 0:
+    v = float(input('Enter a speed greater than 0: '))
+ang = float(input('Enter the projectiles initial angle: '))
+while ang > 90 or ang <-90:
+    ang = float(input('Enter an initial angle above -90 and below 90 degrees: '))
+h = float(input('Enter the projectiles initial height: '))
+while h < 0:
+    h = float(input('Enter a height above 0 Metres: '))
 
+def projectile_motion(v,ang,h,g = 9.81 ):
+    ang = math.radians(ang)
 
-    if height != 0:
-        maaximum_height += height
-#To be finished
+    flight_time = (1/g)*((v * math.sin(ang))+math.sqrt((v*math.sin(ang))**2+(2*g*h)))
+    range_of_flight = v * math.cos(ang) * flight_time
+    maximum_height = (((v*math.sin(ang))**2)/(2*g))+ h
 
     stats_text = (
-        f"Max height: {maximum_height:.2f}\n"
-        f"Range: {range_of_flight:.2f}\n"
-        f"Flight time: {flight_time:.2f}\n"
+        f"Max height: {maximum_height:.2f}m\n"
+        f"Range: {range_of_flight:.2f}m\n"
+        f"Flight time: {flight_time:.2f}s\n"
                   )
-
-
-
-
     def x(t):
-        return (velocity * math.sin(angle))*t
+        return (v * math.cos(ang))*t
     def y(t):
-        return (velocity * math.cos(angle))*t - 1/2*g*(t**2)
+        return (v * math.sin(ang))*t - 1/2*g*(t**2) + h
 
     t=np.linspace(0,flight_time,400)
 
@@ -40,7 +42,5 @@ def projectile_motion(velocity,angle,height = 0, g = 9.81 ):
     plt.show()
 
 
-projectile_motion(10,45)
-
-#to implement height arguement
+projectile_motion(v,ang,h)
 
